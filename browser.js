ceb8ac06ec6a90766660ba59f26c00e416ad58ca
Lauri Rooden <lauri@rooden.ee>
2015-08-09 22:52:14 +0300

    Move everything to inside a closure

--- browser.js
+++ browser.js
@@ -1,116 +1,118 @@
 
+!function(describe) {
 
+	describe.GLOBAL = {}
 
-describe.GLOBAL = {}
-
-describe.it.goTo = function(route) {
-	location.href = "#" + route
-	setTimeout(this.wait(), 60)
-	return this
-}
-
-describe.it.waitTill = function(actual, options) {
-	var result
-	, count = 30
-	, resume = this.wait()
-
-	if (options.timeout) {
-		count = 0 | (options.timeout / 50)
+	describe.it.goTo = function(route) {
+		location.href = "#" + route
+		setTimeout(this.wait(), 60)
+		return this
 	}
 
-	this.ok(function() {
-		return !!result
-	}, options || "Expected: function returns something")
-	test()
+	describe.it.waitTill = function(actual, options) {
+		var result
+		, count = 30
+		, resume = this.wait()
 
-	return this
-
-	function test() {
-		result = actual()
-		if (!result && count--) return setTimeout(test, 50)
-		resume()
-	}
-}
-describe.it.viewOpen = function(actual, options) {
-	return this.waitTill(function() {
-		return View(actual).open
-	}, options || "Expected: View "+actual+" should be open")
-}
-describe.it.waitSelector = function(actual, options) {
-	return this.waitTill(function() {
-		return document.body.find(actual)
-	}, options || "Expected: selector "+actual+" should be in dom")
-}
-describe.it.countSelectors = function(actual, expected, options) {
-	this.waitSelector(actual, options)
-	this.ok(function() {
-		var nodes = document.body.findAll(actual)
-		, count = nodes && nodes.length
-		return count === expected
-	}, options || "Number of matches for " + actual + " should be " + expected)
-	return this
-}
-describe.it.haveText = function(actual, expected, options) {
-	this.waitSelector(actual, options)
-	this.waitTill(function() {
-		var node = document.body.find(actual)
-		, txt = node && node[node.tagName == "INPUT" ? "val" : "txt"]().trim()
-		return txt === expected
-	}, options || actual + " should have text: " + expected)
-	return this
-}
-describe.it.fill = function(actual, expected, options) {
-	this.waitSelector(actual, options)
-	this.ok(function() {
-		var node = document.body.find(actual)
-		, val = node && node.val(expected)
-		return val === expected
-	}, options || actual + " should have value " + expected)
-	return this
-}
-describe.it.click = function(actual, expected, options) {
-	this.waitSelector(actual, options)
-	this.ok(function() {
-		var ev
-		, node = document.body.find(actual)
-		, attr = {
-			pointerX: 0, pointerY: 0, button: 0,
-			ctrlKey: false, altKey: false, shiftKey: false, metaKey: false,
-			bubbles: true, cancelable: true
+		if (options.timeout) {
+			count = 0 | (options.timeout / 50)
 		}
 
-		if (node) {
-			if (node.dispatchEvent) {
-				ev = document.createEvent("MouseEvents")
-				ev.initMouseEvent("click", true, true, document.defaultView, attr.button,
-					attr.pointerX, attr.pointerY, attr.pointerX, attr.pointerY,
-					attr.ctrlKey, attr.altKey, attr.shiftKey, attr.metaKey,
-					attr.button, node)
-				node.dispatchEvent(ev)
-			} else if (node.click) {
-				node.click()
-			} else if (node.fireEvent) {
-				node.fireEvent("onclick")
-			} else if (typeof node.onclick == "function") {
-				node.onclick()
+		this.ok(function() {
+			return !!result
+		}, options || "Expected: function returns something")
+		test()
+
+		return this
+
+		function test() {
+			result = actual()
+			if (!result && count--) return setTimeout(test, 50)
+			resume()
+		}
+	}
+	describe.it.viewOpen = function(actual, options) {
+		return this.waitTill(function() {
+			return View(actual).open
+		}, options || "Expected: View "+actual+" should be open")
+	}
+	describe.it.waitSelector = function(actual, options) {
+		return this.waitTill(function() {
+			return document.body.find(actual)
+		}, options || "Expected: selector "+actual+" should be in dom")
+	}
+	describe.it.countSelectors = function(actual, expected, options) {
+		this.waitSelector(actual, options)
+		this.ok(function() {
+			var nodes = document.body.findAll(actual)
+			, count = nodes && nodes.length
+			return count === expected
+		}, options || "Number of matches for " + actual + " should be " + expected)
+		return this
+	}
+	describe.it.haveText = function(actual, expected, options) {
+		this.waitSelector(actual, options)
+		this.waitTill(function() {
+			var node = document.body.find(actual)
+			, txt = node && node[node.tagName == "INPUT" ? "val" : "txt"]().trim()
+			return txt === expected
+		}, options || actual + " should have text: " + expected)
+		return this
+	}
+	describe.it.fill = function(actual, expected, options) {
+		this.waitSelector(actual, options)
+		this.ok(function() {
+			var node = document.body.find(actual)
+			, val = node && node.val(expected)
+			return val === expected
+		}, options || actual + " should have value " + expected)
+		return this
+	}
+	describe.it.click = function(actual, expected, options) {
+		this.waitSelector(actual, options)
+		this.ok(function() {
+			var ev
+			, node = document.body.find(actual)
+			, attr = {
+				pointerX: 0, pointerY: 0, button: 0,
+				ctrlKey: false, altKey: false, shiftKey: false, metaKey: false,
+				bubbles: true, cancelable: true
 			}
-		}
-		return !!node
-	}, options || actual + " should be clickable")
-	return this
-}
 
-describe.it.collectViewsUsage = function() {
-	var viewsUsage = describe.GLOBAL.viewsUsage = {}
+			if (node) {
+				if (node.dispatchEvent) {
+					ev = document.createEvent("MouseEvents")
+					ev.initMouseEvent("click", true, true, document.defaultView, attr.button,
+						attr.pointerX, attr.pointerY, attr.pointerX, attr.pointerY,
+						attr.ctrlKey, attr.altKey, attr.shiftKey, attr.metaKey,
+						attr.button, node)
+					node.dispatchEvent(ev)
+				} else if (node.click) {
+					node.click()
+				} else if (node.fireEvent) {
+					node.fireEvent("onclick")
+				} else if (typeof node.onclick == "function") {
+					node.onclick()
+				}
+			}
+			return !!node
+		}, options || actual + " should be clickable")
+		return this
+	}
 
-	View.on("show", function(route) {
-		var view = View.views[route]
+	describe.it.collectViewsUsage = function() {
+		var viewsUsage = describe.GLOBAL.viewsUsage = {}
 
-		do {
-			viewsUsage[route] = viewsUsage[route] || 0
-			viewsUsage[route]++
-		} while (route = (view = view.parent || {}).route)
-	})
-	return this
-}
+		View.on("show", function(route) {
+			var view = View.views[route]
+
+			do {
+				viewsUsage[route] = viewsUsage[route] || 0
+				viewsUsage[route]++
+			} while (route = (view = view.parent || {}).route)
+		})
+		return this
+	}
+
+}(describe)
 
